6f91f51f69eaf0af61d023b23e7b5b15c519b2d1
Mojca Colnik <mojca.c13@gmail.com>
2021-03-01 22:17:23 +0100

    Reformat classes to mongoose models

--- src/models/cruise.js
+++ src/models/cruise.js
@@ -1,4 +1,4 @@
-class Cruise {
+/*class Cruise {
     constructor(startDate, endDate, route) {
         this.startDate = startDate;
         this.endDate = endDate;
@@ -16,4 +16,40 @@ class Cruise {
     }
 }
 
-module.exports = Cruise;
+module.exports = Cruise; */
+
+const mongoose = require('mongoose');
+const autopopulate = require('mongoose-autopopulate');
+
+const cruiseSchema = new mongoose.Schema({
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+    route: [
+        {
+            type: Array,
+        }
+    ],
+    passengers: [
+        {
+            type: mongoose.Schema.Types.ObjectId, ref: 'Customer'
+        }
+    ],
+    vacancy: Boolean
+});
+
+cruiseSchema.methods.searchAvailiableCruises = (startDate, endDate, route) => {
+    if (this.vacancy) {
+        this.vacancy = false;
+        return `Listing availiable cruises from ${startDate} to ${endDate} in ${route} route.`
+    } else {
+        return `Sorry, the cruise is sold out.`
+    }
+}
+
+module.exports = mongoose.model('Cruise', cruiseSchema);
--- src/models/customer.js
+++ src/models/customer.js
@@ -1,4 +1,4 @@
-class Customer {
+/*class Customer {
     #idNumber = null;
     #cabinNumber = null;
     #accountNumber = null;
@@ -23,4 +23,41 @@ class Customer {
 }
 //define private variables in advance because of Eslint
 
-module.exports = Customer;
+module.exports = Customer;*/
+
+const mongoose = require('mongoose');
+const autopopulate = require('mongoose-autopopulate');
+
+const customerSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    birthDate: {
+        type: Date,
+        required: true,
+    },
+    idNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    accountNumber: {
+        type: Number,
+        required: true,
+    },
+    profileAccount: Boolean
+});
+
+customerSchema.methods.bookCruise = cruise => {
+    cruise.passengers.push(this);
+}
+
+customerSchema.methods.signUp = (passphrase) => {
+    let username = this.fullName;
+    let password = passphrase;
+    this.profileAccount = true;
+    return `Dear ${username}, welcome aboard!`
+}
+
+module.exports = mongoose.model('Customer', customerSchema);
