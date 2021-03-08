0ff37e71439002bdc1c7b2452970c8f3e991ce3b
Mojca Colnik <mojca.c13@gmail.com>
2021-03-08 21:47:30 +0100

    Set lang to pug in Vue

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
+    div(v-for="cruise in cruises") {{ cruise._id }} leaves on {{ cruise.startDate}}
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios"
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      cruises: []
+    }
+  },
+  async created() {
+    const cruisesRequest = await axios.get('/api/cruises')
+
+    this.cruises = cruisesRequest.data
+  }
+
+}
+</script>
