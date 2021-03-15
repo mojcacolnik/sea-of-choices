0276732755c17852570922f7ed1da07f02c6df75
Mojca Colnik <mojca.c13@gmail.com>
2021-03-15 13:20:37 +0100

    Implement vuex store state, actions and mutations

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<script>
+import { mapActions, mapState } from 'vuex'
+
+export default {
+    name : 'Counter',
+    mounted() {
+        this.incrementCount()
+    },
+    methods: {
+        ...mapActions(['incrementCount'])
+    },
+    computed: {
+        ...mapState(['count'])
+    }
+}
+</script>
+
+<template lang="pug">
+    div
+        p Visitor count: {{ count }}
+</template>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,9 +3,23 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const mutations = {
+  INCREMENT_COUNT: 'incrementCount'
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 0
+  },
+  mutations: {
+    [mutations.INCREMENT_COUNT](state) {
+      state.count++
+    }
+  },
+  actions: {
+    incrementCount(store) {
+      store.commit(mutations.INCREMENT_COUNT)
+    }
+  },
   modules: {}
 })
