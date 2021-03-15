ee3bf930f277b5cf7e2452d73c8e78ee81df8928
Mojca Colnik <mojca.c13@gmail.com>
2021-03-15 13:21:44 +0100

    Update CruiseCard component

---
+++ frontend/src/components/CruiseCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'CruiseCard',
+    props: ['cruise'],
+    data() {
+        return {
+            defaultBio: 'This cruise is pending'
+        }
+    }
+}
+</script>
+
+<template lang="pug">
+.box
+    h2 Sail to: {{ cruise.route }}
+    p Departure: {{ cruise.startDate }}
+    p End: {{ cruise.endDate }}
+    p {{ defaultBio }}
+</template>
+
+<style lang="scss" scoped>
+.box {
+    padding: 2rem;
+    border: 2px solid blue;
+    border-radius: 0.3rem;
+    background: lightskyblue;
+}
+
+
+
+</style>
