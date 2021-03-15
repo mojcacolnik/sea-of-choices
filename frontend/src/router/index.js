a3aee223d2caa01f49c06ca908f0280ff79706a3
Mojca Colnik <mojca.c13@gmail.com>
2021-03-15 13:22:28 +0100

    Introduce CruiseList and CruiseDetail to routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import CruiseList from '../views/CruiseList.vue'
+import CruiseDetail from '../views/CruiseDetail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'CruiseList',
+    component: CruiseList
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/cruises/:id',
+    name: 'CruiseDetail',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: CruiseDetail
   }
 ]
 
