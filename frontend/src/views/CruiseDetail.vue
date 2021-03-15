0c221ee0870cd76908b0d26e085b42b381256c96
Mojca Colnik <mojca.c13@gmail.com>
2021-03-15 13:21:16 +0100

    Update views

---
+++ frontend/src/views/CruiseDetail.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios'
+import CruiseCard from '@/components/CruiseCard.vue'
+import Counter from '@/components/Counter.vue'
+
+export default {
+   name: 'CruiseDetail',
+   components: { CruiseCard, Counter },
+   data() {
+     return {
+       cruise: null
+     }
+   },
+   async created() {
+     const cruisesRequest = await axios.get(`/api/cruises/${this.$route.params.id}`)
+     this.cruise = cruisesRequest.data
+   }
+}
+</script>
+
+<template lang="pug">
+  .about
+    h1 These are the cruise details
+    CruiseCard(v-if="cruise" :cruise="cruise")
+    Counter
+</template>
