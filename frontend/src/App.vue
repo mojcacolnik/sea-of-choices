2802503640f5629f8f316c3291daa8cce3576a71
Mojca Colnik <mojca.c13@gmail.com>
2021-04-05 13:36:35 +0200

    Create navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,16 +23,40 @@ export default {
 <template lang="pug">
   #app
     #nav
-      router-link(to="/") HOME |
-      router-link(to="/register") REGISTER |&nbsp;
-      router-link(to="/login") LOGIN | &nbsp;
-      router-link(to="/profile") PROFILE | &nbsp;
-      a(@click='doLogout') Logout
+      .home
+      h1 Sea Of Choices
+      nav.navbar.navbar-expand-lg.navbar-light.bg-light
+        .container-fluid
+          a.navbar-brand SEA OF CHOICES
+          button.navbar-toggler(type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation')
+            span.navbar-toggler-icon
+          #navbarNav.collapse.navbar-collapse
+            ul.navbar-nav
+              li.nav-item
+                router-link(to="/") Home
+              li.nav-item
+                router-link(to="/ships") Ships
+              li.nav-item
+                router-link(to="/about-us") About Us
+              li.nav-item
+                router-link(to="/store") Store
+              li.nav-item
+                router-link.button.btn.btn-primary(to="/login") Login
+              li.nav-item
+                router-link.button.btn.btn-success(to="/register") Sign Up
+      //- router-link(to="/") HOME |
+      //- router-link(to="/register") REGISTER |&nbsp;
+      //- router-link(to="/login") LOGIN | &nbsp;
+      //- router-link(to="/profile") PROFILE | &nbsp;
+      //- a(@click='doLogout') Logout
     router-view
 
  </template>
 
 <style lang="scss">
+@import '@/assets/theme.scss';
+@import 'bootstrap/scss/bootstrap.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
