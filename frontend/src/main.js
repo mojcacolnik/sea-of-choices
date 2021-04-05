834408b6090be69950c2449841433c2a69c86bb0
Mojca Colnik <mojca.c13@gmail.com>
2021-04-05 13:37:51 +0200

    Import bootstrap and normalize.css to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,14 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import 'normalize.css'
+import 'bootstrap'
+
 Vue.config.productionTip = false
 
 async function main() {
   let storeInstance = await store()
-  
+
   new Vue({
     router: router(storeInstance),
     store: storeInstance,
