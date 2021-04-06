ba30447ebab85d9d09243b643a79a87f1ae9302b
Mojca Colnik <mojca.c13@gmail.com>
2021-04-06 20:18:28 +0200

    Implement bootstrap card for displaying cruises

--- frontend/src/components/CruiseCard.vue
+++ frontend/src/components/CruiseCard.vue
@@ -11,21 +11,31 @@ export default {
 </script>
 
 <template lang="pug">
-.box
-    h2 Cruise: {{ cruise.title }}
-    p Departure: {{ cruise.startDate }}
-    p End: {{ cruise.endDate }}
-    p {{ defaultBio }}
+    .box
+        .row
+            .col-sm-6
+                .card(style='width: 18rem;')
+                    img.card-img-top(src='https://source.unsplash.com/random/150x100' alt='')
+                    .card-body
+                        h5.card-title {{ cruise.title }}
+                        p.card-text
+                            | Some quick example text to build on the card title and make up the bulk of the card's content.
+                    ul.list-group.list-group-flush
+                        li.list-group-item Departure: {{ cruise.startDate}}
+                        li.list-group-item End: {{ cruise.endDate }}
+                        li.list-group-item Route: {{ cruise.route }}
+                    .card-body
+                        router-link.card-link(to="`/cruises/${cruise.id}`") See more...
+                        a.card-link(href='#') BOOK
 </template>
 
 <style lang="scss" scoped>
 .box {
     padding: 2rem;
-    border: 2px solid blue;
+    border: 2px solid rgb(14, 47, 89);
     border-radius: 0.3rem;
-    background: lightskyblue;
+    background: rgb(52, 102, 133);
+    color: lightblue;
 }
-
-
 
 </style>
