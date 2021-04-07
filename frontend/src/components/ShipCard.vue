8fa80a8384b95f56bd61429dfe9c7d557e6932b5
Mojca Colnik <mojca.c13@gmail.com>
2021-04-07 21:33:04 +0200

    Create frontend files for ship displaying

---
+++ frontend/src/components/ShipCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'ShipCard',
+    props: ['ships'],
+    }
+
+</script>
+
+<template lang="pug">
+	.container
+		.card(style='width: 18rem;')
+			img.card-img-top(src='https://source.unsplash.com/random/200x150' alt='ship image')
+			.card-body
+				p.card-text
+					| Some quick example text to build on the card title and make up the bulk of the card's content.
+
+</template>
+
+<style lang="scss" scoped>
+.box {
+    padding: 2rem;
+    border: 2px solid rgb(14, 47, 89);
+    border-radius: 0.3rem;
+    background: rgb(52, 102, 133);
+    color: lightblue;
+}
+
+</style>
---
+++ frontend/src/views/Ships.vue
@@ -0,0 +1,31 @@
+<script>
+import { mapActions } from 'vuex'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Ships',
+  components,
+  data() {
+    return {
+      ships: [],
+    }
+  },
+  async created() {
+    this.ships = await this.fetchShips()
+  },
+  methods: {
+    ...mapActions(['fetchShips'])
+  },
+  computed: {
+      ...mapState([ships])
+  }
+}
+</script>
+
+<template lang="pug">
+  .container
+		h2 Meet the ships:
+		.row
+			.col(v-for="ship in ships")
+				ShipCard(:ship="ship" v-if="ship")
+</template>
