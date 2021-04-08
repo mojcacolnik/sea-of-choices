8d76e883c15605d85020a321719cf88d0d83d039
Mojca Colnik <mojca.c13@gmail.com>
2021-04-08 22:31:06 +0200

    Create views for cruise and ship details

---
+++ frontend/src/views/CruiseDetail.vue
@@ -0,0 +1,59 @@
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "CruiseDetail",
+  components: {},
+  data() {
+    return {
+      cruise: null,
+      backendError: null,
+    };
+  },
+  async created() {
+    this.cruise = await this.fetchCruise(this.$route.params.id)
+  },
+  methods: {
+    ...mapActions(["fetchCruise", "book", "fetchUser"]),
+    async submitBooking(e) {
+      e.preventDefault();
+
+      try {
+        await this.book(this.cruise);
+        this.$router.push("/profile");
+      } catch (err) {
+		  console.log(err)
+        this.backendError = err.response.data.message;
+      }
+    },
+  },
+};
+</script>
+
+<template lang="pug">
+.about(:cruise="cruise" v-if="cruise")
+	h2 {{ cruise.title }}
+	table.table.table-primary
+		tbody
+			tr
+				th Route
+				th Starts
+				th Ends
+			tr
+				td {{ cruise.route }}
+				td {{ cruise.startDate }}
+				td {{ cruise.endDate }}
+	router-link.btn.btn-primary(to="/profile") BACK
+	//- router-link.btn.btn-info(@click="book") BOOK
+	button(@click="submitBooking") BOOK
+
+</template>
+
+<style lang="scss" scoped>
+.about {
+  margin-top: 100px;
+}
+table {
+  width: 100%;
+}
+</style>
